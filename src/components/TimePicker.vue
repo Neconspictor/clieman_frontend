76421e9dda8e3216fd36a16d79df92203915cd77
goeth <goeth@fim.uni-passau.de>
2020-06-27 19:41:30 +0200

    Settings page: Added calendar active working hour

---
+++ src/components/TimePicker.vue
@@ -0,0 +1,101 @@
+<template>
+    <v-menu
+        ref="menuTimePicker"
+        v-model="datePicker"
+        :close-on-content-click="false"
+        :return-value.sync="dateOnlyTime"
+        transition="scale-transition"
+        offset-y
+    >
+        <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+                v-model="dateOnlyTime"
+                :label="label"
+                prepend-icon="access_time"
+                readonly
+                no-details
+                v-bind="attrs"
+                v-on="on"
+                :style="
+                    `width: ${width}; min-width: ${minWidth}; max-width: ${maxWidth}; margin-bottom: 0px;`
+                "
+            ></v-text-field>
+        </template>
+        <v-time-picker
+            v-if="datePicker"
+            v-model="dateOnlyTime"
+            format="24hr"
+            full-width
+            @click:hour="closeTimePicker"
+            @click:minute="$refs.menuTimePicker.save(dateOnlyTime)"
+        ></v-time-picker>
+    </v-menu>
+</template>
+
+<script>
+import DateUtil from '@/util/date.js'
+import moment from 'moment-timezone'
+
+export default {
+    props: {
+        maxWidth: {
+            type: [String, Number],
+            default: 'inherit',
+        },
+        minWidth: {
+            type: [String, Number],
+            default: 'inherit',
+        },
+        width: {
+            type: [String, Number],
+            default: 'inherit',
+        },
+
+        value: {
+            type: Date,
+            required: true,
+        },
+
+        label: {
+            type: String,
+            default: '',
+        },
+    },
+    data() {
+        return {
+            datePicker: false,
+        }
+    },
+
+    computed: {
+        dateOnlyTime: {
+            get: function() {
+                return DateUtil.formatDate(
+                    this.value,
+                    DateUtil.getDefaultTimeZone(),
+                    DateUtil.getHourToMinutesFormat()
+                )
+            },
+
+            set: function(newValue) {
+                const time = moment(newValue, 'HH:mm')
+                const date = moment(new Date(this.value))
+                date.hours(time.hours())
+                date.minutes(time.minutes())
+                this.$emit('input', date.toDate())
+            },
+        },
+    },
+
+    methods: {
+        closeTimePicker: function(v) {
+            v = v < 10 ? '0' + v : v
+            const time = v + ':00'
+            this.menuTimePicker = false
+            this.$refs.menuTimePicker.save(time)
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -27,11 +27,44 @@
                     <v-container class="ml-4 mr-0">
                         <v-row>
                             <v-col>
-                                <div ref="calendar" id="test">
+                                <div ref="calendar">
                                     <h1 class="font-weight-thin">
                                         {{ this.$i18n.t('calendar') }}
                                     </h1>
                                 </div>
+
+                                <v-row>
+                                    <v-col justify-center align-center>
+                                        <v-row class="ml-4" align="center">
+                                            <span class="mr-4 ">
+                                                Active working hours:
+                                            </span>
+                                            <v-row
+                                                style="margin-bottom: -10px;"
+                                            >
+                                                <div>
+                                                    <TimePicker
+                                                        v-model="
+                                                            calendarWorkingHourStart
+                                                        "
+                                                        label="Start"
+                                                        width="100px"
+                                                    />
+                                                </div>
+                                                <div>
+                                                    <TimePicker
+                                                        v-model="
+                                                            calendarWorkingHourEnd
+                                                        "
+                                                        label="End"
+                                                        width="100px"
+                                                    />
+                                                </div>
+                                            </v-row>
+                                        </v-row>
+                                    </v-col>
+                                </v-row>
+
                                 <v-divider></v-divider>
                             </v-col>
                         </v-row>
@@ -44,8 +77,13 @@
 
 <script>
 import $ from 'jquery'
+import moment from 'moment-timezone'
+import TimePicker from '@/components/TimePicker'
 
 export default {
+    components: {
+        TimePicker,
+    },
     data() {
         return {
             items: [
@@ -53,6 +91,9 @@ export default {
                 { title: 'My Account', icon: 'mdi-account' },
                 { title: 'Users', icon: 'mdi-account-group-outline' },
             ],
+
+            calendarWorkingHourStart: moment('08:00', 'HH:mm').toDate(),
+            calendarWorkingHourEnd: moment('18:00', 'HH:mm').toDate(),
         }
     },
 
